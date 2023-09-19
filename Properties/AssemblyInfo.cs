4610fd78fba1c2cd2e8459529ed48f31f556a80f
June Castillote <june.castillote@gmail.com>
2023-09-19 23:28:34 +0800

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GetSPORefreshToken")]
 [assembly: AssemblyCopyright("June Castillote")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("june.castillote@gmail.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
